9196c801a64f563f1ea80d6ae512596b1b60b7ae
jimmy.li <lizhengming@innofidei.com>
2013-05-27 16:53:06 +0800

    usbnet : use noncached buffer for usb transfer
    cache flush seems no works, reason have not debug

--- project/test/usbnet.c
+++ project/test/usbnet.c
@@ -1,3 +1,4 @@
+#define MFLOG_TAG	"usbnet"
 
 #include <string.h>
 #include <stdlib.h>
@@ -15,18 +16,48 @@ extern usbs_composite_serial_t usbs_comp_ser0;
 extern int usbs_comp_serial_rx(usbs_composite_serial_t* ser, void* buf, int n);
 extern void usbs_serials_start(int num);
 
-int usbnet_queue_rx(void * buf, int len)
-{
-//	mf_cache_inv_range(buf, len);
+#define USB_RXBUF_LEN		(2048)
+#define USB_TXBUF_LEN		(2048)
 
-	return usbs_comp_serial_rx(&usbs_comp_ser0, buf, len);
+#define USB_BUF_LEN			(USB_RXBUF_LEN + USB_TXBUF_LEN)
+
+static CYGBLD_ATTRIB_ALIGN(4) unsigned char usb_buffer[USB_BUF_LEN] CYGBLD_ATTRIB_SECTION(".dma");
+
+int usbnet_queue_rx(void *buf, int len)
+{
+	int ret;
+	void *pbuf;
+
+	pbuf = (void*)&usb_buffer[0];
+
+	if (buf == NULL || len <=0 || len > USB_RXBUF_LEN) {
+		MFLOGE("usbnet_queue_rx parameter invalid\n");
+		return 0;
+	}
+
+	ret = usbs_comp_serial_rx(&usbs_comp_ser0, pbuf, len);
+	if (ret > 0) {
+		memcpy(buf, pbuf, ret);
+	}
+
+	return ret;
 }
 
-int usbnet_queue_tx(const void * buf, int len)
+int usbnet_queue_tx(const void *buf, int len)
 {
-	mf_cache_clean_range((void*)buf, len);
+	void *pbuf;
 
-	return usbs_comp_serial_tx(&usbs_comp_ser0, buf, len);
+
+	pbuf = (void*)&usb_buffer[USB_RXBUF_LEN];
+
+	if (buf == NULL || len <= 0 || len > USB_TXBUF_LEN) {
+		MFLOGE("usbnet_queue_tx parameter invalid\n");
+		return 0;
+	}
+
+	memcpy(pbuf, buf, len);
+
+	return usbs_comp_serial_tx(&usbs_comp_ser0, pbuf, len);
 }
 
 int usbnet_init(void)
